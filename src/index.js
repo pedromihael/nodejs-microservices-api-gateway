36853d451c8732f5bb71a2fdbe9da7b144e3dfb3
Pedro Mihael <pedromvpaes@gmail.com>
2021-10-23 17:15:30 -0400

    devops: add network_mode=host in docker-compose to redirect requests to microservices containers

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(cors())
 app.use(logger('dev'))
-app.use('/incidents', httpProxy('http://localhost:3001'))
+app.use('/incidents', httpProxy('http://localhost:3001')) // o host precisa ser o da maquina
 app.use('/projects', httpProxy('http://localhost:3002'))
 app.use('/providers', httpProxy('http://localhost:3003'))
 app.use('/reliabilities', httpProxy('http://localhost:3004'))
@@ -20,6 +20,6 @@ app.get('/health-check', (req, res) => {
   res.status(200).send("Doctor said I'm good.")
 })
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('API Gateway is ready 🚀');
 });
